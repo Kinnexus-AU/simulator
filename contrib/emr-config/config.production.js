f0a4265e52957069bfad8faffa65f0d6d9991b8f
Ilya Beda <ir4y.ix@gmail.com>
2025-08-07 14:47:53 +1000

    Fix production config

--- contrib/emr-config/config.production.js
+++ contrib/emr-config/config.production.js
@@ -1,20 +1,8 @@
 const config = {
     clientId: 'web',
 
-    wearablesAccessConsentCodingSystem: 'https://fhir.emr.beda.software/CodeSystem/consent-subject',
-
     tier: 'production',
-    baseURL: 'https://aidbox.emr.beda.software',
-    sdcIdeUrl: 'https://sdc.beda.software',
-    aiQuestionnaireBuilderUrl: 'https://builder.emr.beda.software',
-
-    sdcBackendUrl: null,
-    webSentryDSN: null,
-    mobileSentryDSN: null,
-    jitsiMeetServer: 'video.emr.beda.software/',
-    wearablesDataStreamService: 'https://ingest.emr.beda.software/api/v1',
-    metriportIdentifierSystem: 'https://api.sandbox.metriport.com',
-    aiAssistantServiceUrl: 'https://scribe.emr.beda.software',
+    baseURL: 'https://aidbox.kinnexus.beda.software',
 };
 
 export { config as default };
